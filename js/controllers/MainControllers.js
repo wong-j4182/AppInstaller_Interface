d097844b9edead4f38e22ab3ba7f76acbb336de3
wong-j4182 <wong.j4182@gmail.com>
2015-08-14 16:43:35 -0400

    Initial commit

---
+++ js/controllers/MainControllers.js
@@ -0,0 +1,58 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.move = {
+    icon: 'img/move.jpg',
+    title: 'MOVE',
+    developer: 'MOVE, Inc.',
+    price: 0.99
+  };
+
+  $scope.shutterbugg = {
+    icon: 'img/shutterbugg.jpg',
+    title: 'Shutterbugg',
+    developer: 'Chico Dusty',
+    price: 2.99
+  };
+
+  $scope.gameboard = {
+    icon: 'img/gameboard.jpg',
+    title: 'Gameboard',
+    developer: 'Armando P.',
+    price: 1.99
+  };
+
+  $scope.forecast = {
+    icon: 'img/forecast.jpg',
+    title: 'Forecast',
+    developer: 'Forecast',
+    price: 1.99
+  };
+  
+  $scope.apps = 
+    [
+    	{
+        icon: 'img/move.jpg',
+        title: 'MOVE',
+        developer: 'MOVE, Inc.',
+        price: 0.99
+      },
+      {
+        icon: 'img/shutterbugg.jpg',
+        title: 'Shutterbugg',
+        developer: 'Chico Dusty',
+        price: 2.99
+      },
+      {
+      	icon: 'img/fallout_shelter.jpg',
+         title: 'Fallout Shelter',
+         developer: 'Bethesda',
+         price: 1.99
+      },
+      {
+      	icon: 'img/hearthstone.jpg',
+         title: 'Hearthstone',
+         developer: 'Blizzard',
+         price: 0.99
+      }
+    ];
+  
+}]);
